23fc3680a1dc3370df71eaaf6899baa65421a52e
Abhishek Naik <an286@njit.edu>
2016-02-15 11:38:42 -0500

    Added the main file to project

---
+++ src/TrieMain.java
@@ -0,0 +1,8 @@
+
+public class TrieMain {
+	public static void main(String[] args) {
+		System.out.println("Find the longest substring within a string");
+		System.out.println("Ex: \nInput: banana \nAnswer: ana");
+	}
+
+}
